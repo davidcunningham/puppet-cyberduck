db4c4ef407c493566d7049946da290d1871da4bd
David Cunningham <david@dcdominion.com>
2014-12-06 18:51:41 -0500

    Updated version to 4.6

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class cyberduck {
   package { 'Cyberduck':
     provider   => 'compressed_app',
-    source     => 'https://update.cyberduck.io/Cyberduck-4.5.2.zip'
+    source     => 'https://update.cyberduck.io/Cyberduck-4.6.zip'
   }
 }
